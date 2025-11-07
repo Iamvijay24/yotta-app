5800bb5c27654a1bc499f00e86fc81d76648ddb0
Dhaya1193 <72986053+Dhaya1193@users.noreply.github.com>
2025-11-07 09:59:07 +0530

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-sass-transformer'),
+  },
+  resolver: {
+    sourceExts: ['js', 'jsx', 'ts', 'tsx', 'scss', 'sass', 'json'],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
